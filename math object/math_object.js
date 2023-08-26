2dd4fb8eec989e27f08bd516039a471c36a6aa0f
shruti <dudhatshruti6@gmail.com>
2023-08-26 21:40:14 +0530

    HELLO

---
+++ math object/math_object.js
@@ -0,0 +1,39 @@
+//  MATHS OBJECT AND PROPERTIES
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);        //square root of 2
+console.log(Math.SQRT1_2);       //square root of 2 and half
+console.log(Math.LN2);            
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+
+// MATHS OBJECTS
+
+console.log(Math.ceil(4.1));        //round up
+console.log(Math.ceil(-4.2));       //round up
+console.log(Math.floor(4.8));       //round down
+console.log(Math.floor(-4.8));      //round down
+console.log(Math.round(4.8));      //rounded
+console.log(Math.round(4.3));      //rounded
+console.log(Math.sqrt(12));         //square root of number
+console.log(Math.pow(2,6));      //   x to power y
+console.log(Math.abs(10.5));      //   absolute value
+console.log(Math.abs(-10));      //   absolute value
+console.log(Math.random());         //random valur (0,1)
+console.log(Math.trunc(14.6));         //return only integer
+console.log(Math.min(2,67,345));         //return minimum value
+console.log(Math.max(2,67,345));         //return maximum value
+
+console.log(Math.log(10));
+console.log(Math.log2(16));
+console.log(Math.log10(1000));
+
+console.log(Math.sign(5));      //return sign
+console.log(Math.sin(90 * Math.PI/180));
+console.log(Math.cos(60 * Math.PI/180));
+console.log(Math.tan(45 * Math.PI/180));
+console.log(Math.tanh(45 * Math.PI/180));
+console.log(Math.cosh(45 * Math.PI/180));
+console.log(Math.sinh(45 * Math.PI/180));
