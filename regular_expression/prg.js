67c116c977a3e74b7a12385cb942d4d8b066136c
shruti <dudhatshruti6@gmail.com>
2023-09-01 13:25:43 +0530

    hello

---
+++ regular_expression/prg.js
@@ -0,0 +1,31 @@
+let data = 'Hello world 125 ,I am learning Java script skillqode@hello.in';
+
+// let reg = /JAVA/i;
+// let result = data.match(reg);
+// console.log(result)
+
+// let reg = /[H,e,5]/g;
+// let result = data.match(reg);
+// console.log(result)
+
+// let reg = /(H|i|@)/g;
+// let result = data.match(reg);
+// console.log(result)
+
+// let reg = /\d/g;
+// let result = data.match(reg);
+// console.log(result)
+
+// let reg = /\s/g;
+// let result = data.match(reg);
+// console.log(result)
+
+// let reg = /\ing\b/g;
+// let result = data.match(reg);
+// console.log(result)
+
+let mo = "915-678-1345";
+let reg = /^(1+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+let result = mo.match(reg);
+console.log(result)
+
