dff45da7405641bcc6f322f03a7ad13c5c417b7e
shruti <dudhatshruti6@gmail.com>
2023-09-02 11:59:45 +0530

    hello

---
+++ basic_function/task_1.js
@@ -0,0 +1,20 @@
+// NO ARGUMENT NO RETURN
+
+function table()
+{
+    let a=7;
+    console.log("table 7::",a*1);
+    console.log("table 7::",a*2);
+    console.log("table 7::",a*3);
+    console.log("table 7::",a*4);
+    console.log("table 7::",a*5);
+    console.log("table 7::",a*6);
+    console.log("table 7::",a*7);
+    console.log("table 7::",a*8);
+    console.log("table 7::",a*9);
+    console.log("table 7::",a*10);
+
+
+}
+
+table();
