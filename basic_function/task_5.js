f9e716216ae32746b3fe391e2b314d3b2de85b6b
shruti <dudhatshruti6@gmail.com>
2023-09-02 12:00:43 +0530

    hello

---
+++ basic_function/task_5.js
@@ -0,0 +1,11 @@
+//  NO ARGUMENT NO RETURN
+
+function chaircost()
+{
+    let totalchair=10,totalcost=55,eachchair=1;
+    eachchair=totalcost/totalchair;
+    console.log("cost is:", eachchair);
+
+}
+
+chaircost();
