4cb234733d4e1515e17dfa53d60a054aac90abc9
shruti <dudhatshruti6@gmail.com>
2023-09-05 23:49:44 +0530

    hello

---
+++ js_function/IIFE_function.js
@@ -0,0 +1,56 @@
+// IIFE FUNCTION
+
+// (function(){
+//     console.log("hello world...");
+// })();
+
+// ((a,b) =>{
+//     console.log("product is:",a+b);
+// })(22,1)
+
+// 1. square of value
+// ((a) => {
+//     console.log("square is:",a*a);
+// })(2)
+
+//2. simple interest
+// ((p,t,r) => {
+//     console.log("SI is:",p*t*r/100);
+// })(2,3,4)
+
+// 3. dollar into rupees
+
+// ((rupees) => {
+//     // dollar = rupees * 75;
+//     console.log("dollar is:",rupees*75);
+// })(20)
+
+
+// 4. chaircost
+// ((totalchair,totalcost,eachchair) => {
+//     console.log("cost is:",eachchair= totalcost/totalchair);
+// })(10,20,5)
+
+// 5. find  l=(c+d)*(g+h);
+
+// ((c,d,g,h) => {
+//     l=(c+d)*(g+h);
+//     l=(c+d)*(g+h);
+//     console.log("dollar is:",l= (c+d) * (g+h));
+// })(20,3,4,5)
+
+// 6. area of circle
+
+// ((pi,r) => {
+//     console.log("area of circle is:",area=pi*r*r);
+// })(3.14,4)
+
+// 7. area of triangle
+
+// ((base,height) => {
+//     console.log("area of triangle is:",area=0.5*base*height);
+// })(14,4)
+
+
+
+
