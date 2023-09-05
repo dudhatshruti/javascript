743509ca02005a6d71c25b2b35f2eb59f25dcc9d
shruti <dudhatshruti6@gmail.com>
2023-09-05 23:51:14 +0530

    hello

---
+++ basic_function/task_14.js
@@ -0,0 +1,19 @@
+// NO ARGUMENT NO RETURN
+
+function add()
+{
+    let a = 65;
+    if(a == 0)
+    {
+        console.log("value is 0:",a)
+    }
+    else if(a>0){
+        console.log("value is positive:",a)
+    }
+    else 
+    {
+        console.log("value is negative:",a)
+    }
+}
+
+add();
