fd165a27750c5c00c0693b815b2f20a9641d77c8
shruti <dudhatshruti6@gmail.com>
2023-09-06 00:12:53 +0530

    hello

---
+++ js_function/this_keyword.js
@@ -0,0 +1,26 @@
+// this keyword
+
+let card = {
+    firstName : "skill",
+    lastName  : "qode",
+    id : 6722,
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+card.fullName.call()
+console.log(card.fullName());
+console.log(card.id);
+console.log(card);
+
+
+
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id : 5566,
+//     fullName : function() {
+//     return this.firstName + " " + this.lastName;
+//     }
+//     }; // John Doe
