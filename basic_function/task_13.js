447246831b12c27438e3a5e07fb48c224b187808
shruti <dudhatshruti6@gmail.com>
2023-09-05 23:51:05 +0530

    hello

---
+++ basic_function/task_13.js
@@ -0,0 +1,15 @@
+// NO ARGUMENT NO RETURN
+
+function one()
+{
+    let a=100, b = 20;
+    if (a<b) {
+        console.log("a is smllest:",a)
+    }
+    else
+    {
+        console.log("b is smallest:",b);
+    }
+}
+
+one();
