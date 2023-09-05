ca79b42d7e0f9c8b1811ea11aebe81ec37b84d9c
shruti <dudhatshruti6@gmail.com>
2023-09-05 23:51:27 +0530

    hello

---
+++ basic_function/task_15.js
@@ -0,0 +1,18 @@
+// NO ARGUMENT WITH RETURN
+
+function one()
+{
+    let a=49,b=15,c;
+    c = a*b;
+    return c;
+}
+let c;
+if(c % 21 == 0)
+    {
+            console.log("divisible",one());  
+    }
+    else{
+        console.log("not divisible",one());  
+    }
+
+// console.log(one());
