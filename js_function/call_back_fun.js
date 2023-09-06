66a0cbabf2af27696333424677b2bcefa24f2f4a
shruti <dudhatshruti6@gmail.com>
2023-09-06 16:29:57 +0530

    hello

---
+++ js_function/call_back_fun.js
@@ -0,0 +1,34 @@
+// CALL BACK FUNCTION
+
+// function hello(){
+//     console.log("how are you...");
+// }
+
+// function skill(s, callback){
+//     console.log("hi...",s);
+//     callback();
+// }
+// skill("peter", hello)
+
+// area of triangle
+
+function hello(base,height){
+    console.log("area of triangle:", 0.5*base*height);
+}
+
+function area(callback){
+    callback(10,20);
+}
+area(hello)
+
+// area of circle
+
+function hii(pi,r)
+{
+    console.log("area of circle:",pi*r*r);
+}
+
+function circle(callback){
+    callback(10,20);
+}
+hii(3.14,4)
