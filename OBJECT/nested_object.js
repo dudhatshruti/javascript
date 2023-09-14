6d1b29851e604d88f73237ce67ef98a9541f45a6
shruti <dudhatshruti6@gmail.com>
2023-09-14 17:15:15 +0530

    hello

---
+++ OBJECT/nested_object.js
@@ -0,0 +1,14 @@
+// 3. NESTED OBJECT 
+
+let person = {
+    name : "skill",
+    age : 25,
+    email: "hello@test.in",
+    adress :{
+        city : "surat",
+        state : "gujrat",
+        country:"india",
+    }
+}
+
+console.log(person.adress.country);
