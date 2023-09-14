bb0ded93d5b43270773487de6f208591157ba96e
shruti <dudhatshruti6@gmail.com>
2023-09-14 17:15:05 +0530

    hello

---
+++ OBJECT/var_object.js
@@ -0,0 +1,22 @@
+//1. object creating as a variable
+
+// let person = {
+//     name : "shruti",
+//     age : 20,
+//     email : "hello@test.com",
+//     hello : function(){
+//         return `${this.name} is ${this.age}`
+//     }
+// };
+
+// // console.log(typeof(person));
+// console.log(person);
+// console.log(person.name);
+// console.log(person.hello());
+// console.log(person["age"]);
+
+
+// let x = "email";
+// console.log(person[x]);
+
+
