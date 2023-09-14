111800e1590eec8320daaade066e0aa69af42ded
shruti <dudhatshruti6@gmail.com>
2023-09-14 17:16:25 +0530

    hello

---
+++ for_in/prg.js
@@ -0,0 +1,29 @@
+//  FOR IN LOOP
+
+// let person = {
+//     name : "hello",
+//     age : 24,
+//     email : "test@hello.in"
+// }
+
+// console.log(Object.keys(person).length);  // return object length
+
+// for(let i in person){
+//     console.log(person[i]);
+// }
+
+
+let abc = ["apple","banana","kiwi","orange"];
+
+for(let x in abc){
+    console.log(abc[x]);
+}
+
+let mystring ="hello how are you?";
+for(let y in mystring){
+    console.log(mystring[y]);
+}
+
+for(let z of mystring){
+    console.log(z);
+}
