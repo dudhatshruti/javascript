c1836f09ed94d7e69d5438d3fc1c4d6fbb4cd146
shruti <dudhatshruti6@gmail.com>
2023-09-18 21:34:27 +0530

    hello

---
+++ SET_TIME_OUT/callback_function.js
@@ -0,0 +1,16 @@
+// CALLBACK FUNCTION
+
+// Program with setTimeout()
+
+function greet(){
+    console.log('hello world...');
+}
+
+function sayName(name){
+    console.log('hello' + ' '+ name);
+}
+
+// calling the function
+
+setTimeout(greet,3000);
+sayName('shruti')
