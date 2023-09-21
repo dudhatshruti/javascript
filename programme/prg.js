2cc44b240d0123d82d5fd47373caeaa18fe5c531
shruti <dudhatshruti6@gmail.com>
2023-09-21 14:09:41 +0530

    hello

---
+++ programme/prg.js
@@ -0,0 +1,29 @@
+//  reverse a number
+
+function hello(x){
+    let y = x.toString;
+    reversedStr = y.split(' ').reverse().join(' ')
+    return reversedStr
+}
+
+let x = 15438;
+let reversed = reverseNumber(x);
+console.log();
+
+function reverseNumber(x) {
+    // Convert the number to a string
+    let str = x.toString();
+    
+    // Reverse the string
+    let reversedStr = str.split('').reverse().join('');
+    
+    // Convert the reversed string back to a number
+    let reversedNumber = parseInt(reversedStr, 10);
+    
+    return reversedNumber;
+  }
+  
+  // Example usage:
+//   let x = 15438;
+//   let reversed = reverseNumber(x);
+//   console.log(reversed); // Output: 83451
